6dacf23085feb89c58377cb17913e8ade414243b
Daniel Laszlo <daniel.laszlo94@gmail.com>
2018-10-14 15:56:00 +0200

    task(shocking): main java file example

--- player58.java
+++ player58.java
@@ -3,9 +3,7 @@ import algorithm.Population;
 import algorithm.initialization.RandomInitialization;
 import algorithm.mutation.NonUniformMutation;
 import algorithm.mutation.UniformMutation;
-import algorithm.parentselection.AllParentSelection;
-import algorithm.parentselection.ParentSelection;
-import algorithm.parentselection.TournamentParentSelection;
+import algorithm.parentselection.*;
 import algorithm.survivalselection.ReplaceAllSurvivalSelection;
 import algorithm.survivalselection.SurvivorSelection;
 import algorithm.survivalselection.TournamentSurvivalSelection;
@@ -80,14 +78,34 @@ public class player58 implements ContestSubmission
 		int tournamentSizeB = 2;
 		double blendAlpha = 0;
 
-		//mutatioin parameters
+		//mutation parameters
 		double probabilityOfMutation = 0.1;
 		double sigma = 0.1;
 		double lowerBoundary = -5.0;
 		double upperBoundary = 5.0;
 
+        // Linear tournament size parameters
+        int tournamentSizeStart = 3;
+        int tournamentSizeEnd = 25;
+        int tournamentSizeGenerations = 3000;
+        int numberOfShocks = 10;
+        int stepsOnMaxSize = 20;
+
+
 		Initialization initialization = new RandomInitialization(populationSize);
-		// Parent selection is moved inside the actual algorithm
+
+		 // Parent selection is moved inside the actual algorithm
+//         ParentSelection parentSelection = new AdaptiveTournamentParentSelection(
+//                tournamentSizeStart, tournamentSizeEnd, tournamentSizeGenerations);
+
+         ParentSelection parentSelection = new ShockedAdaptiveTournamentParentSelection(
+                 tournamentSizeStart,
+                 tournamentSizeEnd,
+                 tournamentSizeGenerations,
+                 numberOfShocks,
+                 stepsOnMaxSize
+                 );
+
 		//UniformMutation mutation = new UniformMutation(probabilityOfMutation,lowerBoundary,upperBoundary);
 		NonUniformMutation mutation = new NonUniformMutation(sigma,lowerBoundary,upperBoundary);
 		Recombination recombination = new BlendRecombination(blendAlpha);
@@ -102,21 +120,11 @@ public class player58 implements ContestSubmission
 		previousGeneration.evaluate(evaluation_);
 		int evals = populationSize;
 
-		int generations = 0;
-
-		// Linear tournament size setter
-		int tournamentSizeStart = 3;
-		int tournamentSizeEnd = 25;
-		int tournamentSizeGenerations = 3000;
-
+        int generation = 0;
         while(evals < evaluations_limit_){
+            // System.out.println(generation++);
 
-			int tournamentSize = (generations * (tournamentSizeEnd - tournamentSizeStart)) / tournamentSizeGenerations + tournamentSizeStart;
-
-			ParentSelection parentSelection = new TournamentParentSelection(tournamentSize);
-
-			System.out.println(evals);
-			onlineFitnessStatisticsPrinter.printStats(previousGeneration);
+			// onlineFitnessStatisticsPrinter.printStats(previousGeneration);
         	// Select parents
 
 			Population parents = parentSelection.selectParents(previousGeneration);
@@ -134,9 +142,6 @@ public class player58 implements ContestSubmission
 
 			evals += nextGeneration.size();
 
-			generations += 1;
-
-			System.out.println(generations);
 
 			previousGeneration = nextGeneration;
 
